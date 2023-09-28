cc07afdd148ee0ad8bf984a06af0a4074673c75f
ryan_achmad <57476576+flutter-ryan@users.noreply.github.com>
2023-09-28 13:35:00 +0800

    initial-commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ryanachmad85/app-say-hello
+
+go 1.20
+
+require github.com/ryanachmad85/go-say-hello v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/ryanachmad85/go-say-hello"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello())
+}
